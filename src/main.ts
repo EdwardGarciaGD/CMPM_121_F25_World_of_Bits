3ffd556981753d693dce18747609871952e68f78
EdwardGarciaGD <egarc216@ucsc.edu>
2025-11-10 02:28:40 +0000

    Added PLAN.md File With D3.a Checklist

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,41 @@
-// @deno-types="npm:@types/leaflet"
 import leaflet from "leaflet";
-
-// Style sheets
-import "leaflet/dist/leaflet.css"; // supporting style for Leaflet
-import "./style.css"; // student-controlled page style
-
-// Fix missing marker images
-import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
-
-// Import our luck function
+import "leaflet/dist/leaflet.css"; // Supporting style for Leaflet
+import "./style.css";
+import "./_leafletWorkaround.ts"; // Fixes for missing Leaflet images
 import luck from "./_luck.ts";
 
-// Create basic UI elements
-
-const controlPanelDiv = document.createElement("div");
-controlPanelDiv.id = "controlPanel";
-document.body.append(controlPanelDiv);
-
-const mapDiv = document.createElement("div");
-mapDiv.id = "map";
-document.body.append(mapDiv);
-
-const statusPanelDiv = document.createElement("div");
-statusPanelDiv.id = "statusPanel";
-document.body.append(statusPanelDiv);
-
-// Our classroom location
-const CLASSROOM_LATLNG = leaflet.latLng(
+// Gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 17;
+const TILE_DEGREES = .9e-4;
+const NEIGHBORHOOD_SIZE = 139;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+const startingLocation = leaflet.latLng(
   36.997936938057016,
   -122.05703507501151,
 );
 
-// Tunable gameplay parameters
-const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const CACHE_SPAWN_PROBABILITY = 0.1;
+// UI elements
+document.title = "Beachcomb the World";
 
-// Create the map (element with id "map" is defined in index.html)
-const map = leaflet.map(mapDiv, {
-  center: CLASSROOM_LATLNG,
+const mapStyle = document.createElement("div");
+mapStyle.id = "map";
+document.body.append(mapStyle);
+
+const statusPanel = document.createElement("div");
+statusPanel.id = "statusPanel";
+document.body.append(statusPanel);
+
+// Map creation
+const map = leaflet.map(mapStyle, {
+  center: startingLocation,
   zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
-  zoomControl: false,
-  scrollWheelZoom: false,
+  minZoom: GAMEPLAY_ZOOM_LEVEL - 3,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL + 1,
+  zoomControl: true,
+  scrollWheelZoom: true,
 });
 
-// Populate the map with a background tile layer
+// Background tile layer
 leaflet
   .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
@@ -56,32 +44,31 @@ leaflet
   })
   .addTo(map);
 
-// Add a marker to represent the player
-const playerMarker = leaflet.marker(CLASSROOM_LATLNG);
-playerMarker.bindTooltip("That's you!");
+// Player marker
+const playerMarker = leaflet.marker(startingLocation);
+playerMarker.bindTooltip("You are here");
 playerMarker.addTo(map);
 
-// Display the player's points
-let playerPoints = 0;
-statusPanelDiv.innerHTML = "No points yet...";
+// Player's inventory
+let playerInventory = 0;
+statusPanel.innerHTML = "You are holding nothing";
 
-// Add caches to the map by cell numbers
+// Adds caches to the map by cell numbers
 function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
+  const origin = startingLocation;
   const bounds = leaflet.latLngBounds([
     [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
     [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
   ]);
 
-  // Add a rectangle to the map to represent the cache
+  // Adds a rectangle to the map to represent the cache
   const rect = leaflet.rectangle(bounds);
   rect.addTo(map);
 
   // Handle interactions with the cache
   rect.bindPopup(() => {
     // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 5);
 
     // The popup offers a description and button
     const popupDiv = document.createElement("div");
@@ -96,18 +83,17 @@ function spawnCache(i: number, j: number) {
         pointValue--;
         popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
           pointValue.toString();
-        playerPoints++;
-        statusPanelDiv.innerHTML = `${playerPoints} points accumulated`;
+        playerInventory++;
+        statusPanel.innerHTML = `${playerInventory} points accumulated`;
       });
 
     return popupDiv;
   });
 }
 
-// Look around the player's neighborhood for caches to spawn
+// Player's neighborhood cache spawn
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
     if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
       spawnCache(i, j);
     }
